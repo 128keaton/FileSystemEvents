63e167240fe070d8c172acce6e1cd410f287bcd9
Keaton Burleson <keaton.burleson@me.com>
2016-10-06 10:03:56 -0500

    Initial Commit

---
+++ FileSystemEvents/FileSystemEvents.h
@@ -0,0 +1,19 @@
+//
+//  FileSystemEvents.h
+//  FileSystemEvents
+//
+//  Created by Keaton Burleson on 10/6/16.
+//  Copyright © 2016 Keaton Burleson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FileSystemEvents.
+FOUNDATION_EXPORT double FileSystemEventsVersionNumber;
+
+//! Project version string for FileSystemEvents.
+FOUNDATION_EXPORT const unsigned char FileSystemEventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileSystemEvents/PublicHeader.h>
+
+
