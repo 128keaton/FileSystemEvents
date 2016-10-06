aa32692cc2bc57c566164e6cf763706aa779b1b5
Keaton Burleson <keaton.burleson@me.com>
2016-10-06 10:23:30 -0500

    Initial Commit

---
+++ EonilFileSystemEvents/EonilFileSystemEvents.h
@@ -0,0 +1,19 @@
+//
+//  EonilFileSystemEvents.h
+//  EonilFileSystemEvents
+//
+//  Created by Keaton Burleson on 10/6/16.
+//  Copyright © 2016 Keaton Burleson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EonilFileSystemEvents.
+FOUNDATION_EXPORT double EonilFileSystemEventsVersionNumber;
+
+//! Project version string for EonilFileSystemEvents.
+FOUNDATION_EXPORT const unsigned char EonilFileSystemEventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EonilFileSystemEvents/PublicHeader.h>
+
+
